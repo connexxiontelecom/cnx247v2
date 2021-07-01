6e3c4519be72972c58420c545dbd506fb88c8d81
Joseph <talktojoegee@gmail.com>
2021-07-01 14:20:54 +0100

    run business process

--- resources/views/backend/workflow/common/_run-business-process.blade.php
+++ resources/views/backend/workflow/common/_run-business-process.blade.php
@@ -9,6 +9,9 @@
                 <a class="dropdown-item waves-light waves-effect" href="{{ route('business-trip') }}"><i class="ti-notepad mr-2"></i> Business Trip </a>
                 <a class="dropdown-item waves-light waves-effect" href="{{ route('leave-request') }}"><i class="ti-calendar mr-2"></i> Leave Approval </a>
                 <a class="dropdown-item waves-light waves-effect" href="{{ route('internal-memo') }}"><i class="ti-pin-alt mr-2"></i> Internal Memo </a>
+							@can('workflow business process')
+								<a class="dropdown-item waves-light waves-effect" href="{{ route('workflow-business-process') }}"><i class="ti-pin-alt mr-2 text-danger"></i> Workflow Business Process </a>
+							@endcan
             </div>
         </div>
     </div>
